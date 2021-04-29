8fede9cf75b673f4bace46f0edffbda26435962d
Anish Sethiya <anishsethiya22@gmail.com>
2021-04-29 19:34:15 +0530

    Crud With Rest

--- RestSpringCrudHateos/src/main/java/com/example2/RestSpringBoot/RestSpringBoot2/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example2.RestSpringBoot.RestSpringBoot2;
-
-class EmployeeNotFoundException extends RuntimeException {
-
-  EmployeeNotFoundException(Long id) {
-    super("Could not find employee " + id);
-  }
-}
