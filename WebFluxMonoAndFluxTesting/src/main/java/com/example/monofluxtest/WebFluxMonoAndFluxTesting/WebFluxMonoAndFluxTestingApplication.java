d74ccf98b8632f5e92290ae2ee9c01d413de1a27
Anish Sethiya <anishsethiya22@gmail.com>
2021-05-14 18:11:53 +0530

    WebFlux Mono And Flux Testing

---
+++ WebFluxMonoAndFluxTesting/src/main/java/com/example/monofluxtest/WebFluxMonoAndFluxTesting/WebFluxMonoAndFluxTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.monofluxtest.WebFluxMonoAndFluxTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxMonoAndFluxTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxMonoAndFluxTestingApplication.class, args);
+	}
+
+}
